c89ac57270a4dc18ca16fe1e65cf51e4d7183b47
Donatussss <don.sp4r@gmail.com>
2026-01-03 16:17:14 +0300

    Updated w25qxx and fixed some bugs

--- Inc/w25q_lfs.h
+++ Inc/w25q_lfs.h
@@ -1,3 +1,6 @@
+#ifndef INC_W25Q_LFS_H_
+#define INC_W25Q_LFS_H_
+
 #include "lfs.h"
 #include "w25qxx.h"
 
@@ -5,3 +8,6 @@ int w25q_lfs_device_init(w25q_device_t *w25q_device);
 int w25q_lfs_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
 int w25q_lfs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
 int w25q_lfs_sector_erase(const struct lfs_config *c, lfs_block_t block);
+int w25q_lfs_sync(const struct lfs_config *c);
+
+#endif /* INC_W25Q_LFS_H_ */
--- Src/w25q_lfs.c
+++ Src/w25q_lfs.c
@@ -5,6 +5,7 @@ static w25q_device_t *dev = NULL;
 int w25q_lfs_device_init(w25q_device_t *w25q_device)
 {
     dev = w25q_device;
+    return SPI_OK;
 }
 
 int w25q_lfs_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size)
@@ -16,7 +17,7 @@ int w25q_lfs_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, 
 int w25q_lfs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size)
 {
     uint32_t addr = (block * (c->block_size)) + off;
-    return w25q_write(dev, addr, NULL, size);
+    return w25q_write(dev, addr, buffer, size);
 }
 
 int w25q_lfs_sector_erase(const struct lfs_config *c, lfs_block_t block)
@@ -27,6 +28,6 @@ int w25q_lfs_sector_erase(const struct lfs_config *c, lfs_block_t block)
 
 int w25q_lfs_sync(const struct lfs_config *c)
 {
-    UNUSED(c);
+    (void)c;
     return LFS_ERR_OK;
 }
